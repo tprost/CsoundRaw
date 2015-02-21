6c25dac49ff5619e7f136016a7518c2f3275e060
oldmanmike <oldmanmike.dev@gmail.com>
2015-02-21 15:31:34 -0500

    Fleshed out Instantiation.

--- src/Sound/Csound/Instantiation.hs
+++ src/Sound/Csound/Instantiation.hs
@@ -9,16 +9,32 @@
 --
 -------------------------------------------------------------------------------
 module Sound.Csound.Instantiation (
+    csoundInitialize,
+    csoundCreate,
+    csoundDestroy,
     csoundGetAPIVersion,
     csoundGetVersion
 ) where
 
 import Control.Monad.IO.Class
 import Foreign.C
+import Foreign.Ptr
 import Foreign.C.Types
 
+foreign import ccall "csound.h csoundInitialize" csoundInitialize' :: CInt -> IO CInt
+foreign import ccall "csound.h csoundCreate" csoundCreate' :: Ptr () -> IO (Ptr ())
+foreign import ccall "csound.h csoundDestroy" csoundDestroy' :: Ptr () -> IO ()
 foreign import ccall "csound.h csoundGetAPIVersion" csoundGetAPIVersion' :: IO CInt
 foreign import ccall "csound.h csoundGetVersion" csoundGetVersion' :: IO CInt
+
+csoundInitialize :: MonadIO m => CInt -> m CInt
+csoundInitialize flag = liftIO (csoundInitialize' flag)
+
+csoundCreate :: MonadIO m => Ptr () -> m (Ptr ())
+csoundCreate hostdata = liftIO (csoundCreate' hostdata)
+
+csoundDestroy :: MonadIO m => Ptr () -> m ()
+csoundDestroy csoundptr = liftIO (csoundDestroy' csoundptr)
 
 csoundGetAPIVersion :: MonadIO m => m CInt
 csoundGetAPIVersion = liftIO csoundGetAPIVersion'
